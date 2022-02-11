f969357cd91d6259113ea751a1d978557a57c62d
he4rtbleed <he4rtbleed@pm.me>
2022-02-11 03:33:25 +0000

    15552 - 빠른 A+B

---
+++ 15552/main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+	cin.tie(nullptr)->sync_with_stdio(false);
+	
+	int N;
+	cin >> N;
+	
+	while (N--) {
+		int A, B;
+		cin >> A >> B;
+		cout << A + B;
+	}
+}
