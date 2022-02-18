d22d8ca38db6d6d7ba66e69aebd79fb1f71d3922
he4rtbleed <he4rtbleed@pm.me>
2022-02-18 13:21:34 +0000

    1008 - A/B

---
+++ 1008.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+	cin.tie(nullptr)->sync_with_stdio(false);
+	
+	double A, B;
+	cin >> A >> B;
+	
+	cout.precision(12);
+	cout << A / B;
+}
